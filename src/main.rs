5d915ecb92387d987041c76d3d92b5fa2a82e640
Iqbal <aodihis@gmail.com>
2025-01-19 20:59:28 +0700

    * Add some styling for message, and url part.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,25 @@ fn App() -> Html {
             <div class="header">
                 <h1>{"Websocket Playground"}</h1>
             </div>
-            <div>
-                <form>
-                    <input type="text" id="text" />
-                </form>
+            <div class="body">
+                <div class="input">
+                    <div class="url-input-container">
+                        <input type="text" id="url" placeholder="ws://websocket.url"/>
+                        <button class="button hidden primary" type="submit">{"Connect"}</button>
+                        <button class="button danger" type="submit">{"Disconnect"}</button>
+                    </div>
+                    <div class="message">
+                        <div class="message-header">
+                            <label for="message">{"Message"}</label>
+                            <button class="button danger">{"Send"}</button>
+                        </div>
+                        <textarea type="text" id="message" />
+                    </div>
+                </div>
+                <div class="output">
+                    <div class="data-sum"></div>
+                    <div class="data-detail"></div>
+                </div>
             </div>
         </>
     }
